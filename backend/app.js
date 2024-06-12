fb8678f47e8b134751267364cb2756611a2fe87a
Parth Gupta <neelaru8@gmail.com>
2024-06-12 16:42:56 +0530

    1st commit

---
+++ backend/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const uri = process.env.URI.replace("<password>", process.env.PASSWORD);
+
+mongoose.connect(uri).then(() => {
+  console.log(`connected to the database`);
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+app.use((req, res) => {
+  res.status(404).json({
+    error: "Not Found",
+    message: "ServiceTrack has no such route.",
+  });
+});
